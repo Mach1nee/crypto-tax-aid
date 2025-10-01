785c54a07f3eed8ef5264c91f0b2ab418e0b2d2c
Mach1nee <machine@Machine.localdomain>
2025-10-01 04:29:01 -0300

    commit

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,9 @@
-// src/integrations/supabase/client.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://pejtkofyaluozogtakzp.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlanRrb2Z5YWx1b3pvZ3Rha3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyODM2NDUsImV4cCI6MjA3NDg1OTY0NX0.W1wKSJmBb3Xzlx1E79y7NcoqTeATR6GX4WF4CMIP39E";
+const SUPABASE_URL = "https://pejtkofyaluozogtakzp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlanRrb2Z5YWx1b3pvZ3Rha3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyODM2NDUsImV4cCI6MjA3NDg1OTY0NX0.W1wKSJmBb3Xzlx1E79y7NcoqTeATR6GX4WF4CMIP39E";
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
