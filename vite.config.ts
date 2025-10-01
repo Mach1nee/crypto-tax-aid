0af30ed59eb91dde8236da410bdc268a452b6cc4
Mach1nee <machine@Machine.localdomain>
2025-09-30 22:44:59 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // <- adicione esta linha
   server: {
     host: "::",
     port: 8080,
